c9c60535c37119822a28cfd01fb7419832abc1de
Jaime Perez <jperezresume@gmail.com>
2022-12-09 22:07:02 -0500

    path changes

--- v3/cmd/grab/main.go
+++ v3/cmd/grab/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/cavaliergopher/grab/v3/pkg/grabui"
+	"github.com/jpmicrosoft/grab/v3/pkg/grabui"
 )
 
 func main() {
--- v3/grab_test.go
+++ v3/grab_test.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/cavaliergopher/grab/v3/pkg/grabtest"
+	"github.com/jpmicrosoft/grab/v3/pkg/grabtest"
 )
 
 func TestMain(m *testing.M) {
